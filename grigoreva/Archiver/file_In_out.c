9fcfca441f088e6ececc30742d428804bc32bd20
Григорьева Анастасия <51454241+SourMandarin@users.noreply.github.com>
2021-04-27 16:01:33 +0300

    Add files via upload

--- grigoreva/Archiver/file_In_out.c
+++ grigoreva/Archiver/file_In_out.c
@@ -3,7 +3,17 @@
 
 void reading_from_file(FILE* fp, symbol* simbols, int* kolvo, int* kk, int* k)
 {
-    // :)
+    if ((fp = fopen(argv[1], "r+")) == NULL)
+    {
+    	printf("Error\n");
+    	return 0;
+    }
+    
+    i = 0;
+    while ((simbols[i] = fgetc(fp)) != EOF)
+    {
+    	
+    }
 }
 
 void writing_to_file(FILE* fp2 , FILE* fp3, symbol* simbols, int* kolvo, int* kk, int* k, int *fsize2)
